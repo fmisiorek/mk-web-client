4de5df5e852599cf429e80335597c045cb0448b7
Filip Misiorek <3535701+fmisiorek@users.noreply.github.com>
2021-10-03 22:02:47 +0200

    Initial commit for mk-web-client

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="App logo" src="./assets/logo.png" />
+    <WelcomeMK msg="Explore status of coffee machines in the office!" />
+  </div>
+</template>
+
+<script>
+import WelcomeMK from "./components/Welcome.vue";
+
+export default {
+  name: "App",
+  components: {
+    WelcomeMK,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#logo {
+  width: 150px;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="welcome">
+    <h1>{{ msg }}</h1>
+    <p>
+      It's a good place to start your working day...
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeMK',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
